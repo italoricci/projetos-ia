3f49efdea55ded3397a6e86a668148e6bdfec0c9
Italo Ferreira <italoricci@gmail.com>
2026-06-08 19:06:04 -0300

    chore: incluindo exemplo de service-token nos mcps + rate limit em api legadas.

--- mcp/mcp-service-tokens/customers-mcp/getServiceToken.sh
+++ mcp/mcp-service-tokens/customers-mcp/getServiceToken.sh
@@ -1,7 +1,7 @@
 SERVICE_TOKEN=$(
   curl -X POST http://localhost:9999/v1/auth/service-token \
   -H "Content-Type: application/json" \
-  -d '{"username": "erickwendel", "password": "123123", "adminSuperSecret": "AM I THE BOSS?"}' \
+  -d '{"username": "erickwendel", "password": "123123", "adminSuperSecret": "super-secret-admin-token"}' \
   | jq -r '.serviceToken')
 
 echo "AdminService Token: $SERVICE_TOKEN"
@@ -9,10 +9,10 @@ echo "AdminService Token: $SERVICE_TOKEN"
 
 SERVICE_TOKEN=$(curl --silent -X POST http://localhost:9999/v1/auth/service-token \
   -H "Content-Type: application/json" \
-  -d '{"username": "ananeri", "password": "1234", "adminSuperSecret": "AM I THE BOSS?"}' \
+  -d '{"username": "ananeri", "password": "1234", "adminSuperSecret": "super-secret-admin-token"}' \
   | jq -r '.serviceToken')
 
 echo "Member Service Token: $SERVICE_TOKEN"
 
 curl http://localhost:9999/v1/customers \
-  -H "Authorization: Bearer $SERVICE_TOKEN"
+  -H "X-Service-Token: $SERVICE_TOKEN"
